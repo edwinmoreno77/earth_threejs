e8214fe780328e7e934300531652f8e8392f5921
Edwin Moreno <edwinmoreno77@gmail.com>
2025-03-09 20:16:57 -0300

    earth img added

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/earth_threejs/", // Replace with your repository name
+  base: "/earth_threejs/",
   plugins: [react()],
 });
