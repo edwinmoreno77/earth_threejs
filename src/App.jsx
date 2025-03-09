910320da8e21b588d7eaf683ce6686bb940ed33b
Edwin Moreno <edwinmoreno77@gmail.com>
2025-03-08 21:50:32 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PlanetScene from "./components/PlanetScene";
+
+function App() {
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <PlanetScene />
+    </div>
+  );
+}
+
+export default App;
